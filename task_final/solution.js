4eacfbf47ca0beca642c3647cf6035b2a436e76c
Zorin Alex <ZorinAlex1992@yandex.ru>
2023-04-15 16:06:34 +0300

    task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -21,33 +21,39 @@
 //    "data": {
 //      "client": "Иван +7(987)65-43-210",
 //      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв. 53",
+//        "address": "ул Ленина, дом 2, 4 подъезд, 5 этаж, кв. 53",
 //        "sum": 900
 //       },
 //       "goods": [
 //         {
 //           "title": "Пицца",
 //           "count": 2
-//         }
+//         } 
+
 //      ]
 //    }
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: {}, order: {}, goods: []};
     let countOfGoods = goods.length;
-
     for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        if (typeof goods[i] == "object"){
+            data.goods.push(
+                {
+                    title: goods[i].title,
+                    count: goods[i].count
+                }
+            );
+        }
     }
+    data.client = `${name} ${phone}`;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
